2cbf80353e0c869a09104e7212300bd9a20f30f3
suntnaja <sunt8346@gmail.com>
2026-09-17 01:42:56 +0700

    Update app.js

--- train/app.js
+++ train/app.js
@@ -81,6 +81,24 @@ async function fetchHistoricalWeather(datetimeStr) {
             conditions_text: err.message 
         };
     }
+
+    function rgbToHsv(r, g, b) {
+    r /= 255; g /= 255; b /= 255;
+    let max = Math.max(r, g, b), min = Math.min(r, g, b);
+    let h, s, v = max;
+    let d = max - min;
+    s = max === 0 ? 0 : d / max;
+    if (max == min) { h = 0; } else {
+        switch (max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+    return [Math.round(h * 179), Math.round(s * 255), Math.round(v * 255)];
+    }
+    
 }
 
 // ==========================================
