67f57af548ae72d6649f5cb4cb987da2c12343ee
suntnaja <sunt8346@gmail.com>
2026-09-22 15:28:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app) # อนุญาตให้ HTML ยิง API เข้ามาได้
 
 # โหลดไฟล์ โมเดล .pkl (สมมติชื่อ model.pkl)
-with open('model.pkl', 'rb') as file:
+with open('data/sky_weather_rf_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
 @app.route('/predict', methods=['POST'])
